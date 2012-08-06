17a1a55765a6159e69098f1a0287ca53b855ea05
Vsevolod Romashov <7@7vn.ru>
2012-08-07 03:12:00 +0400

    basic tabbed layout, other small changes

--- main.rb
+++ main.rb
@@ -17,14 +17,15 @@ class Messenger
   def start
     Fiber.new do
       friends = $client.friends.get(fields: [:screen_name, :photo])
-      @ws.send Oj.dump(friends)
+      @ws.send Oj.dump('type' => 'friends_list', 'data' => friends)
       
       params = $client.messages.get_long_poll_server
+      # @ws.send Oj.dump(params)
       url = 'http://' + params.delete(:server)
       params.update(act: 'a_check', wait: 25, mode: 2)
       
       while response = VkontakteApi::API.connection.get(url, params).body
-        @ws.send Oj.dump(response)
+        @ws.send Oj.dump('type' => 'updates', 'data' => response.updates)
         params[:ts] = response.ts
       end
     end.resume
@@ -38,7 +39,7 @@ EM.synchrony do
       messenger = Messenger.new(ws)
       messenger.start
       
-      ws.send 'Hello Client.'
+      # ws.send 'Hello Client.'
     end
     
     ws.onclose do
--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,29 @@
-$(document).ready(function() {
-  function debug(str) {
-    console.log(str);
-  };
+var usersList = {
+  list: [],
   
+  load: function(list) {
+    this.list = list;
+  }
+};
+
+$(document).ready(function() {
   ws = new WebSocket("ws://0.0.0.0:8080");
   ws.onmessage = function(evt) {
-    $("#msg").append("<pre>" + evt.data + "</pre>");
+    $("#debug").append("<pre>" + evt.data + "</pre>");
+    
+    var message = $.parseJSON(evt.data);
+    console.log('received message from server:');
+    console.log(message);
+    
+    if(message.type == 'friends_list') {
+      usersList.load(message.data);
+    }
   };
   ws.onopen = function() {
-    debug("connected...");
-    ws.send("hello server");
+    console.log("connected...");
+    // ws.send("hello server");
   };
   ws.onclose = function() {
-    debug("socket closed");
+    console.log("socket closed");
   };
 });
