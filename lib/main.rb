d706fa3a2b99392df87d4854e5cd892571d33a6e
Vsevolod Romashov <7@7vn.ru>
2012-09-09 02:35:11 +0400

    comments in main.rb

--- lib/main.rb
+++ lib/main.rb
@@ -8,22 +8,28 @@ require_relative 'messenger'
 $stdout.sync = true
 
 VkontakteApi.configure do |config|
-  config.adapter         = :em_synchrony
+  # совершаем запросы через em_synchrony-адаптер
+  config.adapter = :em_synchrony
+  # в основном цикле получения сообщений соединения будут
+  # висеть до 25 секунд, поэтому ставим таймаут на полминуты
   config.faraday_options = { request: { timeout: 30 } }
-  config.log_responses   = true
 end
 
+# создаем клиент API, через него будем отправлять все запросы к ВКонтакте
 $client = VkontakteApi::Client.new(ENV['TOKEN'])
 
 EM.synchrony do
   EventMachine::WebSocket.start(host: '0.0.0.0', port: 8080) do |ws|
     ws.onopen do
+      # при открытии соединения с браузером создаем новый мессенджер
       puts 'Connection open'
       $messenger = Messenger.new(ws)
       $messenger.start
     end
     
     ws.onclose do
+      # при закрытии соединения останавливаем мессенджер,
+      # чтобы он перестал запрашивать обновления
       $messenger.stop
       puts 'Connection closed'
     end
